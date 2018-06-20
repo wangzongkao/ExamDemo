bd39dc9621ded19056af48e64f09d5dae202e7b3
wangzongkao <15168265016@139.com>
2018-06-20 21:54:25 +0800

    wangzongkao

--- src/main/java/com/migu/schedule/Schedule.java
+++ src/main/java/com/migu/schedule/Schedule.java
@@ -4,51 +4,125 @@ package com.migu.schedule;
 import com.migu.schedule.constants.ReturnCodeKeys;
 import com.migu.schedule.info.TaskInfo;
 
+import java.beans.beancontext.BeanContext;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /*
-*类名和方法不能修改
+ *类名和方法不能修改
  */
 public class Schedule {
 
-
+    private static int initialValue = 1;
+    Map<Integer, TaskInfo> map = new  HashMap<Integer, TaskInfo>();
+    Map<Integer, Integer> cost = new HashMap<Integer, Integer>();
     public int init() {
-        // TODO 方法未实现
+        map.clear();
+        if(map.size() == 0)
+        {
+            return ReturnCodeKeys.E001;
+        }
         return ReturnCodeKeys.E000;
     }
 
 
     public int registerNode(int nodeId) {
-        // TODO 方法未实现
-        return ReturnCodeKeys.E000;
+        if(nodeId>0)
+        {
+            if(map.get(nodeId) == null)
+            {
+                if(map.size()==0)
+                {
+                    map.put(initialValue, new TaskInfo());
+                }
+                else
+                {
+                    map.put(nodeId, new TaskInfo());
+                }
+                return ReturnCodeKeys.E003;
+            }
+            else
+            {
+                return ReturnCodeKeys.E005;
+            }
+        }
+        else
+        {
+            return ReturnCodeKeys.E004;
+        }
     }
 
     public int unregisterNode(int nodeId) {
-        // TODO 方法未实现
-        return ReturnCodeKeys.E000;
+        TaskInfo taskInfo = map.remove(nodeId);
+        if(taskInfo == null)
+        {
+            return ReturnCodeKeys.E007;
+        }
+        else
+        {
+            return ReturnCodeKeys.E006;
+        }
     }
 
 
     public int addTask(int taskId, int consumption) {
-        // TODO 方法未实现
-        return ReturnCodeKeys.E000;
+
+        if(taskId<=0)
+        {
+            return ReturnCodeKeys.E009;
+        }
+        if(cost.containsKey(taskId))
+        {
+            return ReturnCodeKeys.E010;
+        }
+        cost.put(taskId, consumption);
+        return ReturnCodeKeys.E008;
     }
 
 
     public int deleteTask(int taskId) {
-        // TODO 方法未实现
-        return ReturnCodeKeys.E000;
+        if(taskId<=0)
+        {
+            return ReturnCodeKeys.E009;
+        }
+        if(cost.containsKey(taskId))
+        {
+            cost.remove(taskId);
+            return ReturnCodeKeys.E011;
+        }
+        else
+        {
+            return ReturnCodeKeys.E012;
+        }
+
     }
 
 
     public int scheduleTask(int threshold) {
-        // TODO 方法未实现
-        return ReturnCodeKeys.E000;
+        int x = cost.size();
+        List<HashMap<Integer, Integer>> list = new ArrayList<HashMap<Integer,Integer>>();
+        if(threshold>0&&threshold<=10)
+        {
+            return ReturnCodeKeys.E013;
+        }
+        else if(threshold >10)
+        {
+            return ReturnCodeKeys.E014;
+        }
+        else
+        {
+            return ReturnCodeKeys.E002;
+        }
     }
 
 
     public int queryTaskStatus(List<TaskInfo> tasks) {
-        // TODO 方法未实现
+        if(tasks == null)
+        {
+            return ReturnCodeKeys.E016;
+        }
         return ReturnCodeKeys.E000;
     }
 
